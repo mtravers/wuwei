255599d9cc9195d7dd7181f71b304feabbf6899e
Mike Travers <mt@collabrx.com>
2010-01-26 14:23:27 -0800

    merge autobob->nlistener, fixup for unit test automation
    --HG--
    branch : nlistener

--- package.lisp
+++ package.lisp
@@ -2,6 +2,8 @@
 (defpackage :wuwei
   (:nicknames :wu)
   (:use :cl :mt :3utils
-        :net.aserve :net.html.generator)
+        :net.aserve :net.html.generator
+	:lisp-unit)
   (:export #:uploader #:*file-field-name*)
+  (:shadowing-import-from :mt "SET-EQUAL")
   )
