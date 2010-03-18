06a73f3e4cb15dcb712222bafc029ce07d360bbd
Mike Travers <mt@collabrx.com>
2010-03-18 14:48:04 -0700

    c1lite packaged and delivered
    --HG--
    branch : c1lite

--- package.lisp
+++ package.lisp
@@ -1,8 +1,8 @@
-
 (defpackage :wuwei
   (:nicknames :wu)
   (:use :cl :mt :3utils
         :net.aserve :net.html.generator
+	:clos*				;added for c1lite
 	:lisp-unit)
   (:export #:uploader #:*file-field-name*)
   (:shadowing-import-from :mt "SET-EQUAL")
