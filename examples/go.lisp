07c9d97da852a71f3162b22382b6b05e8f8f1167
Mike Travers <mt@alum.mit.edu>
2012-03-18 18:30:08 -0700

    ok im an idiot
    --HG--
    branch : heroku

--- examples/go.lisp
+++ examples/go.lisp
@@ -1,6 +1,6 @@
 (in-package :wu)
 
 (defun cl-user::initialize-application ()
-  (locate-public-directory "./wupub/")
+  (locate-public-directory "./public/")
   (setf *developer-mode* (equal (ccl:getenv "DEVELOPER_MODE") "Y")))
 
