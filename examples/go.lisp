81084c99dff956938846a6cd46b5108902a91d4e
Mike Travers <mt@alum.mit.edu>
2012-03-18 18:22:08 -0700

    getting read faults??? try a meaningless rebuild
    --HG--
    branch : heroku

--- examples/go.lisp
+++ examples/go.lisp
@@ -1,6 +1,6 @@
 (in-package :wu)
 
 (defun cl-user::initialize-application ()
-  (wu:locate-public-directory "./wupub/")
+  (locate-public-directory "./wupub/")
   (setf *developer-mode* (equal (ccl:getenv "DEVELOPER_MODE") "Y")))
 
