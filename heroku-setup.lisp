dfe10812ff096e3af39ba6a809ffcb600ef64cae
Mike Travers <mt@alum.mit.edu>
2012-04-24 08:48:19 -0700

    at least site is up, punting for now, i have real work to do

--- heroku-setup.lisp
+++ heroku-setup.lisp
@@ -6,10 +6,9 @@
 ;(asdf:clear-system "wuwei-examples")
 
 ;(load (make-pathname :directory *build-dir* :defaults "wuwei.asd"))
+;;; This is NOT WORKING, and the build gets Wuwei from Quicklisp.  But that's better than what it was doing before, which was using a completely obsolete version...no fucking idea what is going on here.
 (push (make-pathname :directory *build-dir*) asdf:*central-registry*)
 
 (ql:quickload :wuwei-examples)
-
-(trace wu::wuwei-initialize-application wu::locate-public-directory cl-user::initialize-application)
 
 (print ">>> Done building system")
